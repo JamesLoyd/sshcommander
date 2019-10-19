b1f3e3f0d5eaa58b73e097c2d97afd7058b0ba87
James Loyd <jamestloyd@gmail.com>
2019-10-19 12:37:32 -0400

    added config and removed class 1

---
+++ SSHCommander.Core/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SSHCommander.Core
+{
+    public class Config
+    {
+        public string KeyLocation { get; set; }
+        public string Username { get; set; }
+        public string IPAddress { get; set; }
+    }
+}
